bd83c0dfdd4fc49bef055390ff5bf0adcba08808
pisonpeakgroup <pisonpeakgroup@gmail.com>
2018-03-02 12:45:46 +0100

    Update config.js

--- Config/config.js
+++ Config/config.js
@@ -1,1 +1,21 @@
 
+// For  database.config.js in config folder
+module.exports = {
+    url: 'mongodb://localhost:1000/schooldata'
+}
+
+
+// For server.js after app.use bodyperser
+// Configuring the database
+var dbConfig = require('./config/database.config.js');
+var mongoose = require('mongoose');
+
+mongoose.connect(dbConfig.url);
+
+mongoose.connection.on('error', function() {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+mongoose.connection.once('open', function() {
+    console.log("Successfully connected to the database");
+})
