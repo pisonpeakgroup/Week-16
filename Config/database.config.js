6a1cb2714360b984d9d29726c4e69cf94331d462
pisonpeakgroup <pisonpeakgroup@gmail.com>
2018-03-07 15:35:16 +0100

    Update database.config.js

--- Config/database.config.js
+++ Config/database.config.js
@@ -1,21 +1,15 @@
-
 // For  database.config.js in config folder
 module.exports = {
-    url: 'mongodb://localhost:1000/schooldata'
+  url: 'mongodb://<pisonpeak>:<musa08058702618>@ds253468.mlab.com:53468/week16'
+};
+{
+    "_id"; "week16.pisonpeak",
+    "user"; "pisonpeak",
+    "db"; "week16",
+    "roles"; [
+        {
+            "role": "dbOwner",
+            "db": "week16"
+        }
+    ]
 }
-
-
-// For server.js after app.use bodyperser
-// Configuring the database
-var dbConfig = require('./config/database.config.js');
-var mongoose = require('mongoose');
-
-mongoose.connect(dbConfig.url);
-
-mongoose.connection.on('error', function() {
-    console.log('Could not connect to the database. Exiting now...');
-    process.exit();
-});
-mongoose.connection.once('open', function() {
-    console.log("Successfully connected to the database");
-})
