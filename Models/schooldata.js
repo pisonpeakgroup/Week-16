d9029a92bc7a09bad4f73493893379df27706208
pisonpeakgroup <pisonpeakgroup@gmail.com>
2018-03-13 13:10:07 +0100

    Update schooldata.js

--- Models/schooldata.js
+++ Models/schooldata.js
@@ -1,27 +1,68 @@
+//Mongoose Schema
 
-// Basic MONGOOSE'S Schema   
-   const mongoose = require('mongoose');
+import bcrypt from 'bcryptjs';
+import mongoose from 'mongoose';
+import shortid from 'shortid';
 
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
-    title: {
-        type: String
-    },
-    author: {
-        type: String
-    },
-    yearOfPublication: {
-        type: Number
-    },
-    description: {
-        type: String
-    },
-    isbn: {
-        type: String
-    }
+const userSchema = new Schema({
+  _id: {
+    type: String,
+    default: shortid.generate
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  dateOfBirth: {
+    type: Date,
+    default: Date.now
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female', 'Decline'],
+    default: 'Decline'
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  profilePhoto: {
+    type: String
+  },
+  joined: {
+    type: Date,
+    default: Date.now
+  },
+  role: {
+    type: Number,
+    ref: 'Role',
+    default: 1
+  }
 });
 
-const bookModel = mongoose.model('Schooldata', bookSchema);
+userSchema.virtual('password').get(function () {
+  return this.hashedPassword;
+});
 
-module.exports = bookModel;
+userSchema.virtual('password').set(function (plainText) {
+  const salt = bcrypt.genSaltSync(10);
+  this.hashedPassword = bcrypt.hashSync(plainText, salt);
+});
+
+const userModel = mongoose.model('User', userSchema);
+
+export default userModel;
