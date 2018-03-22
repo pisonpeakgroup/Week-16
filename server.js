40ef72c4082609ee5478708eba3803460da9b2f6
pisonpeakgroup <pisonpeakgroup@gmail.com>
2018-03-22 11:04:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,41 @@
+// require express as a dependecy
 var express = require('express');
-var bodyParser = require('body-parser');
+
+// require body-parser as a dependency
+ bodyParser = require('body-parser');
 
 // create express app
 var app = express();
-
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+ app.use(bodyParser.urlencoded({ extended: true }));
+ // parse requests of content-type - application/json 
+  app.use(bodyParser.json());
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
 
-// define a simple route
+// Configuring the database in server.js
+var mongoose = require('mongoose');
+var db = require('./config/db.js');
+
+mongoose.connect(db.url);
+mongoose.connection.on('error', function() {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+mongoose.connection.once('open', function() {
+   })
+ console.log("Successfully connected to the database");
+
+// definning a simple route
 app.get('/', function(req, res){
     res.json({"message": "Welcome to School Data application. Take notes quickly. Organize and keep track of all your notes."});
 });
-
-// A put route
-app.put('/', function(req, res){
-    res.json({"message": "Welcome to the part where you can put info. Organize and keep track of all your informations."});
-});
-
-// A post route
-app.post('/', function(req, res){
-    res.json({"message": "Welcome to the School Data application part where all data are posted. Organize and keep track of all your data."});
-});
-
-// A delete route
-app.delete('/', function(req, res){
-    res.json({"message": "Welcome to the School Data application. Here you quickly delete unwanted data. Organize and keep track of all your work."});
-});
-
+// Require user routes UserRoute.js
+var UserRoute = require('./routes/UserRoute.js');
 // listen for requests
-app.listen(1000, function(){
-    console.log("Server is listening on port 1000");
+app.listen(3000, function(){
+    console.log("Server is listening on port 3000, so two down few to go!");
 });
+
+// We use module.exports to make this app object visible to the rest of the program
+//after calling for it using require() 
+module.exports = app;
