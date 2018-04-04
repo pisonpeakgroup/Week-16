adb0e2f46612353c60da0ae253a43423d0e0e2ca
pisonpeakgroup <pisonpeakgroup@gmail.com>
2018-04-04 14:51:26 +0100

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -1,3 +1,6 @@
+// require mongoose
+var mongoose = require('mongoose');
+//mongodb default server for connection
 module.exports = {
-    url: 'mongodb://localhost:27017/easy-notes'
+    url: 'mongodb://localhost:27017'
 }
