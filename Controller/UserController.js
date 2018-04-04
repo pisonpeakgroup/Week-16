d2642fee38b0b7f577de029ae02333e3dc3076d5
pisonpeakgroup <pisonpeakgroup@gmail.com>
2018-04-04 14:50:40 +0100

    Update UserController.js

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,53 +1,56 @@
+var app = require('express')();
+
+// UserController.js
+var UserControllers  = require('express').Router();
 
 
-var user = require('../models/User.js');
-
+// CREATES A NEW USER
 exports.create = function(req, res) {
     // Create and Save a new Note
-       if(!req.body.content) {
-        res.status(400).send({message: "User can not be empty"});
+    if(!req.body.content) {
+        res.status(400).send({message: "Note can not be empty"});
     }
-    var User = new User({title: req.body.title || "Untitled User", content: req.body.content});
+    var usercontrollers = new UserControllers({title: req.body.title || "Untitled Note", content: req.body.content});
 
-    user.save(function(err, data) {
+    usercontrollers.save(function(err, data) {
         console.log(data);
         if(err) {
             console.log(err);
-            res.status(500).send({message: "Some error occurred while creating the User."});
+            res.status(500).send({message: "Some error occurred while creating the Note."});
         } else {
             res.send(data);
         }
     });
 };
 
+ // Retrieve and return all notes from the database.
 exports.findAll = function(req, res) {
-    // Retrieve and return all notes from the database.
-    user.find(function(err, user){
+       UserControllers.find(function(err, notes){
         if(err) {
-            res.status(500).send({message: "Some error occurred while retrieving user."});
+            res.status(500).send({message: "Some error occurred while retrieving notes."});
         } else {
-            res.send(notes);
+            res.send(usercontrollers);
         }
     });
 };
 
-exports.findOne = function(req, res) {
     // Find a single note with a noteId
-    user.findById(req.params.userId, function(err, data) {
+exports.findOne = function(req, res) {
+    UserControllers.findById(req.params.userId, function(err, data) {
         if(err) {
             res.status(500).send({message: "Could not retrieve note with id " + req.params.userId});
         } else {
             res.send(data);
         }
     });
-
 };
 
+
+// Update a note identified by the noteId in the request
 exports.update = function(req, res) {
-    // Update a note identified by the noteId in the request
-     User.findById(req.params.userId, function(err, user) {
+       UserControllers.findById(req.params.userId, function(err, user) {
         if(err) {
-            res.status(500).send({message: "Could not find a note with id " + req.params.userId});
+            res.status(500).send({message: "Could not find a note with id " + req.params.noteId});
         }
 
         user.title = req.body.title;
@@ -55,23 +58,23 @@ exports.update = function(req, res) {
 
         user.save(function(err, data){
             if(err) {
-                res.status(500).send({message: "Could not update note with id " + req.params.userId});
+                res.status(500).send({message: "Could not update note with id " + req.params.noteId});
             } else {
                 res.send(data);
             }
         });
     });
-
 };
 
+// Delete a note with the specified noteId in the request
 exports.delete = function(req, res) {
-    // Delete a note with the specified noteId in the request
-    User.remove({_id: req.params.userId}, function(err, data) {
+        UserControllers.remove({_id: req.params.userId}, function(err, data) {
         if(err) {
             res.status(500).send({message: "Could not delete note with id " + req.params.id});
         } else {
             res.send({message: "Note deleted successfully!"})
         }
     });
-
 };
+
+module.exports = UserControllers;
