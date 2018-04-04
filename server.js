63a4eea35868344b4088f680c0942f406311114f
pisonpeakgroup <pisonpeakgroup@gmail.com>
2018-04-04 14:47:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-// require express as a dependecy
+// require express as a dependecy to create our app
 var express = require('express');
+//require mongoose as a dependency to create a database connection
+var mongoose = require('mongoose');
+// require body-parser as a dependency to parse our requests in urlencoded content type
+bodyParser = require('body-parser');
 
-// require body-parser as a dependency
- bodyParser = require('body-parser');
+
 
 // create express app
 var app = express();
@@ -12,11 +15,8 @@ var app = express();
   app.use(bodyParser.json());
 
 
-// Configuring the database in server.js
-var mongoose = require('mongoose');
-var db = require('./config/db.js');
 
-mongoose.connect(db.url);
+//creating the databese connecion using mongoose
 mongoose.connection.on('error', function() {
     console.log('Could not connect to the database. Exiting now...');
     process.exit();
@@ -24,18 +24,27 @@ mongoose.connection.on('error', function() {
 mongoose.connection.once('open', function() {
    })
  console.log("Successfully connected to the database");
+ 
+// Configuring the database in server.js to access db.js in config folder
+var db = require('./config/db.js');
+
+//configuring the route from UserRoutes.js
+var app = require('express')();
+var UserRoutes = require('./Routes/UserRoutes.js');
+var UserControllers = require('./Controllers/UserControllers.js');
+//  Connect all our routes to our application
+app.use('/', UserRoutes);
+
+
+//server port and console message
+app.listen(3000, function(){
+    console.log("Server is listening on port 3000, so two down few to go!");
+
 
 // definning a simple route
 app.get('/', function(req, res){
     res.json({"message": "Welcome to School Data application. Take notes quickly. Organize and keep track of all your notes."});
 });
-// Require user routes UserRoute.js
-var UserRoute = require('./routes/UserRoute.js');
-// listen for requests
-app.listen(3000, function(){
-    console.log("Server is listening on port 3000, so two down few to go!");
-});
-
 // We use module.exports to make this app object visible to the rest of the program
-//after calling for it using require() 
 module.exports = app;
+});
